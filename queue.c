0bbd0a7bb5520e65fe7483c23756a351e95b1efb
BennyWang1007 <bennywang1007@gmail.com>
2025-03-02 18:41:30 +0800

    Implement q_delete_mid
    q_delete_mid() deletes the middle node in the queue by:
    
    - Returning false if the head is NULL or the queue is empty.
    - Using fast-slow pointers to find the middle node.
    - Deleting the middle node and freeing its associated memory.
    
    Change-Id: I1be092e39b49e529d4d4ef5c1b20724afa8653ca

--- queue.c
+++ queue.c
@@ -132,6 +132,21 @@ int q_size(struct list_head *head)
 bool q_delete_mid(struct list_head *head)
 {
     // https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/
+    if (!head || list_empty(head))
+        return false;
+
+    struct list_head *slow = head->next, *fast = slow;
+
+    while (fast != head && fast->next != head) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    /* Slow is the middle node */
+    list_del(slow);
+    free(list_entry(slow, element_t, list)->value);
+    free(list_entry(slow, element_t, list));
+
     return true;
 }
 
