0113a774659f964244799a874055385968d9b56a
BennyWang1007 <bennywang1007@gmail.com>
2025-03-02 18:23:37 +0800

    Implement q_size
    q_size() returns the number of elements in the queue by:
    - Returning 0 if the head is NULL.
    - Iterating through the queue and counting the elements.
    - Returning the final count.
    
    Change-Id: I911c4f1c0e48565f73306897fe681870a8d1028e

--- queue.c
+++ queue.c
@@ -117,7 +117,15 @@ element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 /* Return number of elements in queue */
 int q_size(struct list_head *head)
 {
-    return -1;
+    if (!head)
+        return 0;
+
+    int count = 0;
+
+    for (struct list_head *node = head->next; node != head; node = node->next)
+        count++;
+
+    return count;
 }
 
 /* Delete the middle node in queue */
